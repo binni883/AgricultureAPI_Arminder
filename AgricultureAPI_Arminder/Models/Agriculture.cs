3560f96fc55e2d76d932e566ab39bb3c80a2662e
binni883@gmail.com <binni883@gmail.com>
2021-04-14 09:34:06 +0300

    Add project files.

---
+++ AgricultureAPI_Arminder/Models/Agriculture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgricultureAPI_Arminder.Models
+{
+    public class Agriculture
+    {
+        public int ID { get; set; }
+        public string CropName { get; set; }
+        public string CropPrice { get; set; }
+        public string CropSeed { get; set; }
+    }
+}
